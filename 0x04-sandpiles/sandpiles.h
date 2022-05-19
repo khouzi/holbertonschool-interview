c184c9cecc7cb431164170d40f9a59e35bd193ff
khouzi <khouzem.boussaid@gmail.com>
2022-05-19 22:55:31 +0200

    sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,4 +1,4 @@
-#ifndef __SANDPILES__LIB__
+fndef __SANDPILES__LIB__
 #define __SANDPILES__LIB__
 
 #include <stdlib.h>
@@ -7,5 +7,7 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
+int verif(int grid[3][3]);
 
 #endif
+
