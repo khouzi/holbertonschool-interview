91204b12c520e2678af1cd83221ae60288725818
khouzi <khouzem.boussaid@gmail.com>
2023-02-16 15:09:49 +0100

    0x0C-nqueens/C

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,39 +1,73 @@
 #!/usr/bin/python3
-""" program that solves the N queens problem """
+""" nqueens """
 import sys
 
 
-def isSafe(Board, line, i):
-    """ checks if we can insert queen at column i in that line in Board"""
-    for x in range(line):
-        if (Board[x] == i or
-                Board[x] + line - x == i or
-                Board[x] + x - line == i):
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+N = int(sys.argv[1])
+board = [[0 for i in range(N)]for j in range(N)]
+k = 1
+
+
+def print_sol(board):
+    global k
+    k = k + 1
+    S = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                S.append([i, j])
+    print(S)
+
+
+def isSafe(board, row, col):
+    for i in range(col):
+        if (board[row][i]):
             return False
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if(board[i][j]):
+            return False
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < N:
+        if(board[i][j]):
+            return False
+        i = i + 1
+        j = j - 1
     return True
 
 
-def Fill_line(Board, line):
-    """ fills each line of the board with the correct index """
-    for i in range(len(Board)):
-        if isSafe(Board, line, i):
-            Board[line] = i
-            if line < len(Board) - 1:
-                Fill_line(Board, line + 1)
-            else:
-                print([[i, Board[i]] for i in range(len(Board))])
+def solving(board, col):
+    if (col == N):
+        print_sol(board)
+        return True
+    res = False
+    for i in range(N):
+        if (isSafe(board, i, col)):
+            board[i][col] = 1
+            res = solving(board, col + 1) or res
+            board[i][col] = 0
+    return res
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-try:
-    n = int(sys.argv[1])
-except:
-    print("N must be a number")
-    sys.exit(1)
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
 
-Board = [-1 for i in range(n)]
-Fill_line(Board, 0)
+def solve():
+    board = [[0 for j in range(N)]for i in range(N)]
+    if (solving(board, 0) is False):
+        print("Solution does not exist")
+        return
+    return
+
+
+solve()
